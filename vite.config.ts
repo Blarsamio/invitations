3888c5648aeeaf1f77149b6688342938e28ed455
Patricio <patriciopherrero@gmail.com>
2023-05-23 09:26:03 +0200

    trying dist as output directory for rollup

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,23 @@
-import {defineConfig} from 'vite'
+import { defineConfig } from 'vite'
 import FullReload from "vite-plugin-full-reload"
 import RubyPlugin from 'vite-plugin-ruby'
 import StimulusHMR from 'vite-plugin-stimulus-hmr'
 
 export default defineConfig({
-      clearScreen: false,
-      plugins: [
-          RubyPlugin(),
-          StimulusHMR(),
-          FullReload(["config/routes.rb", "app/views/**/*"], {delay: 300}),
-      ],
-
+  clearScreen: false,
+  plugins: [
+    RubyPlugin(),
+    StimulusHMR(),
+    FullReload(["config/routes.rb", "app/views/**/*"], { delay: 300 }),
+  ],
+  build: {
+    manifest: true,
+    rollupOptions: {
+      output: {
+        format: 'es',
+        strict: false,
+        dir: 'dist/'
+      }
     }
-)
+  }
+})
